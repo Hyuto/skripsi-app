73302950ad4e92c3ba6194db79a5bb4ba3edf581
Hyuto <wahyusetianto19@gmail.com>
2022-05-02 20:06:57 +0700

    Update ort-wasm every start or build command

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # Skripsi Application
 
 Skripsi application to test model trained on research live in the browser.
-
-## Setup
-
-```bash
-yarn install && yarn setup
-```
--- ort-wasm-handler.js
+++ ort-wasm-handler.js
@@ -1,12 +1,15 @@
 const fs = require("fs");
 const path = require("path");
 
-// copy onnxruntime-web WebAssembly files to {workspace}/public/ folder
+// copy onnxruntime-web WebAssembly files to public/ folder
 const srcFolder = path.join(__dirname, "node_modules", "onnxruntime-web", "dist");
 const destFolder = path.join(__dirname, "public", "static", "js");
-if (!fs.existsSync(destFolder)) {
-  fs.mkdirSync(destFolder, { recursive: true });
+
+if (fs.existsSync(destFolder)) {
+  fs.rmSync(destFolder, { recursive: true, force: true });
 }
+fs.mkdirSync(destFolder, { recursive: true });
+
 fs.copyFileSync(path.join(srcFolder, "ort-wasm.wasm"), path.join(destFolder, "ort-wasm.wasm"));
 fs.copyFileSync(
   path.join(srcFolder, "ort-wasm-simd.wasm"),
