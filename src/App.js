5043d64667b99220063d24fde8ea2992f5ce62ac
Wahyu Setianto <wahyusetianto19@gmail.com>
2022-09-16 00:06:43 +0700

    Changing icos and develop UI to show pred value

--- src/App.js
+++ src/App.js
@@ -1,31 +1,71 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import { modelHandler } from "./modelHandler";
+import { Pressable, Text, TextInput, View } from "react-native";
+import Description from "./components/description";
+import Card from "./components/card";
+//import { modelHandler } from "./modelHandler";
 
 const App = () => {
-  const model = new modelHandler();
+  // const model = new modelHandler();
+  const [text, setText] = useState("");
+  const [prediction, setPrediction] = useState(null);
 
   useEffect(() => {
-    model.loadSession().then(async () => {
+    /* model.loadSession().then(async () => {
       const words = "jokowi mau makan";
       const result = await model.predict(words);
       console.log(result);
-    });
+    }); */
   }, []);
 
   return (
     <View className="flex-1 bg-white">
-      <View className="flex-1 items-center justify-between mx-4 mt-8 mb-6">
-        <View className="flex flex-row items-center justify-between w-full">
+      <View className="flex-1 items-center justify-between mx-4 mt-9 lg:mt-4 mb-4">
+        <View className="flex-row items-center w-full">
           <View className="px-2.5 pt-1.5 pb-2 bg-black rounded-lg">
             <Text className="text-3xl text-white font-semibold">Showcase</Text>
           </View>
         </View>
-        <View className="flex flex-row items-center justify-between">
-          <Text className="text-lg">Hello from </Text>
-          <View className="px-2.5 pt-1.5 pb-2 bg-black rounded-lg">
-            <Text className="text-lg text-yellow-400 italic font-semibold">src/App.js!</Text>
+        <View className="flex items-center w-full">
+          <Description />
+          <View className="flex w-full max-w-[550px] my-4">
+            <TextInput
+              onChangeText={setText}
+              value={text}
+              placeholder="Any words to detect?"
+              multiline
+              className="p-3 mx-5 border rounded text-md text-center"
+            />
+            <View className="flex-row justify-center my-4">
+              <Pressable
+                className="bg-black p-2.5 pt-3 rounded mx-2"
+                onPress={() => {
+                  // TODO: Implement detect()
+                  setPrediction(text);
+                }}
+              >
+                <Text className="text-white font-bold">Predict</Text>
+              </Pressable>
+              <Pressable
+                className="bg-transparent border-2 border-black p-2.5 rounded mx-2"
+                onPress={() => {
+                  setText("");
+                  setPrediction(null);
+                }}
+              >
+                <Text className="font-bold">Reset</Text>
+              </Pressable>
+            </View>
+            {prediction && (
+              <View className="flex-row justify-center flex-wrap mt-2">
+                <Card emoji="😲" label="Surprise" percentage={70.5216} />
+                <Card emoji="😲" label="Surprise" percentage={70} />
+                <Card emoji="😲" label="Surprise" percentage={70} />
+                <Card emoji="😲" label="Surprise" percentage={70} />
+                <Card emoji="😲" label="Surprise" percentage={70} />
+                <Card emoji="😲" label="Surprise" percentage={70} />
+              </View>
+            )}
           </View>
         </View>
         <View>
